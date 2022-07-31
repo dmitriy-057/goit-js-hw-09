c8e155176a45822f2730b3c69537d26e9d0a62f2
Dmitriy Gonharov <my.webpage057@gmail.com>
2022-08-01 00:58:36 +0300

    3st task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,50 @@
+// import Notiflix from 'notiflix';
+
+
+
+const form = document.querySelector('.form')
+console.log(form)
+const firstDelay = document.querySelector('[name=delay]')
+console.log(firstDelay)
+const delayStep = document.querySelector("input[name=step]")
+const amount = document.querySelector("input[name=amount]")
+const btn = document.querySelector("button")
+btn.addEventListener("click", onBtnClick)
+
+function onBtnClick(e) {
+  e.preventDefault()
+  for(let position = 0; position <= amount.value; position+=1) {
+    let del = delayStep.value;
+    console.log(del);
+
+    createPromise(position, delayStep.value)
+    .then(({ position, delay }) => {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+
+  }
+
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  setInterval(() => {
+    return new Promise((resolve, reject) => {
+  
+    if (shouldResolve) {
+      // Fulfill
+    return true
+    } else {
+      // Reject
+      return false
+    }
+})
+  }, delayStep.value);
+ 
+
+
 }
+
